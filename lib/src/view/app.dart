fa76fe43265bf4b17bd9793da143d2385ba13978
seungryeol <opop0421@naver.com>
2023-02-17 14:55:32 +0900

    리스트 예외처리

--- lib/src/view/app.dart
+++ lib/src/view/app.dart
@@ -8,7 +8,9 @@ import 'package:pop_up_store/src/model/repository/PopUp-Provider.dart';
 
 class HomeScreen extends StatelessWidget {
 
-
+const HomeScreen({
+  Key? key
+}):super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +24,10 @@ class HomeScreen extends StatelessWidget {
                       itemCount: popup_data.length,
                       itemBuilder: (context, index){
                         final popdata_list = popup_data[index];
+
+                        if(popdata_list == null){
+                          return Container();
+                        }
                         return Padding(
                             padding: const EdgeInsets.only(bottom: 5,left: 5,right: 5),
                             child:GestureDetector(
